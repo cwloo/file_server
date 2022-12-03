3f64823c7c6d27ddb6c1a1afc80038629d5274b1
ro <luochengwu888@gmail.com>
2022-12-03 16:44:25 +0800

    commit

--- file_server/timer_handler.go
+++ file_server/timer_handler.go
@@ -22,8 +22,8 @@ func handlerExpiredFile() {
 	}))
 }
 
+// 清理长期未访问的已上传文件记录
 func checkExpiredFile() {
-	////// 清理长期未访问的已上传文件记录
 	fileInfos.RangeRemoveWithCond(func(info *FileInfo) bool {
 		if info.Ok() && info.Md5Ok {
 			return time.Since(info.HitTime()) >= time.Duration(FileExpiredTimeout)*time.Second
@@ -34,17 +34,18 @@ func checkExpiredFile() {
 	})
 }
 
+// 清理未决的任务，对应移除未决或校验失败的文件
 func checkPendingUploader() {
 	switch UseAsyncUploader {
 	case true:
-		////// 清理未决的任务，对应移除未决或校验失败的文件(异步)
+		////// 异步
 		uploaders.Range(func(_ string, uploader Uploader) {
 			if time.Since(uploader.Get()) > time.Duration(PendingTimeout)*time.Second {
 				uploader.NotifyClose()
 			}
 		})
 	default:
-		////// 清理未决的任务，对应移除未决或校验失败的文件(同步)
+		////// 同步
 		uploaders.RangeRemoveWithCond(func(uploader Uploader) bool {
 			return time.Since(uploader.Get()) > time.Duration(PendingTimeout)*time.Second
 		}, func(uploader Uploader) {
