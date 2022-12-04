ac7bd0da2e04ed20f0cf44d1514433b3badf270b
ro <luochengwu888@gmail.com>
2022-12-04 20:08:29 +0800

    commit

--- file_server/params.go
+++ file_server/params.go
@@ -42,10 +42,10 @@ func checkAlltotal(total int64) bool {
 	return total < MaxAllTotalSize
 }
 
-func checkMultiPartSizeLimit(header *multipart.FileHeader) bool {
-	return header.Size < MaxSegmentSize
-}
-
 func checkMultiPartSize(header *multipart.FileHeader) bool {
 	return header.Size > 0
+}
+
+func checkMultiPartSizeLimit(header *multipart.FileHeader) bool {
+	return header.Size < MaxSegmentSize
 }
