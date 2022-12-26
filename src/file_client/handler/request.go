b3d556a515c38697e852310bd37a6ca2da0d643d
ro <luochengwu888@gmail.com>
2022-12-26 12:39:02 +0800

    commit

--- src/file_client/handler/request.go
+++ src/file_client/handler/request.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"math/rand"
 	"mime/multipart"
 	"net/http"
 	"os"
@@ -20,46 +19,6 @@ import (
 	"github.com/cwloo/uploader/src/global"
 	"github.com/cwloo/uploader/src/global/httpcli"
 )
-
-func GetRouter(client *http.Client, md5 string) string {
-	method := "GET"
-	r := rand.Int() % len(config.Config.Client.Addr)
-	url := strings.Join([]string{
-		config.Config.Client.Addr[r].Proto, "://",
-		config.Config.Client.Addr[r].Ip, ":",
-		strconv.Itoa(config.Config.Client.Addr[r].Port),
-		config.Config.Client.Path.Router}, "")
-	logs.Tracef("request =>> %v %v", method, url+"?md5="+md5)
-	res, err := client.Get(url + "?md5=" + md5)
-	if err != nil {
-		logs.Errorf(err.Error())
-		return ""
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		logs.Errorf(err.Error())
-		return ""
-	}
-	if len(body) == 0 {
-		return ""
-	}
-	resp := global.RouterResp{}
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		logs.Errorf(err.Error())
-		logs.Warnf("%v", string(body))
-		return ""
-	}
-	switch resp.ErrCode {
-	case 0:
-		if resp.Node.Domain == "" {
-			logs.Fatalf("error")
-		}
-		logs.Tracef("%v", string(body))
-		return resp.Node.Domain
-	}
-	return ""
-}
 
 func Upload() {
 	_, filelist := parseargs()
